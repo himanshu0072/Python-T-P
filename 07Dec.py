2e0523e273c9f0e339d6ca8da96ed5797478fb85
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-12-07 23:31:11 +0530

    learning basic of python

--- 07Dec.py
+++ 07Dec.py
@@ -1,6 +1,52 @@
-dic1={"mac":'123', 'john':'456', "mary":'789', "joe":'000', "jane":'111', "jill":'222', "jack":'333', "james":'444', "jim":'555', "jenny":'666', "jerry":'777', "julia":'888', "joseph":'999'}
-dic2={}
-for key, value in dic1.items():
-    print(key, value)
-    dic2[value]=key
-print(dic2)
+'''Dictionary'''
+# dic1={"mac":'123', 'john':'456', "mary":'789', "joe":'000', "jane":'111', "jill":'222', "jack":'333', "james":'444', "jim":'555', "jenny":'666', "jerry":'777', "julia":'888', "joseph":'999'}
+# dic2={}
+# for key, value in dic1.items():
+#     print(key, value)
+#     dic2[value]=key
+# print(dic2)
+
+'''List'''
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# print(list[0:15])
+
+'''Tuple'''
+# tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(tuple[0:10])
+
+'''Set'''
+# set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+# print(set)
+
+'''Dictionary'''
+# dic1={"mac":'123', 'john':'456', "mary":'789', "joe":'000', "jane":'111', "jill":'222', "jack":'333', "james":'444', "jim":'555', "jenny":'666', "jerry":'777', "julia":'888', "joseph":'999'}
+# print(dic1)
+
+'''Iterate'''
+# for i in range(1, 11):
+#     print(i)
+
+'''break continue pass'''
+'''break'''
+# for _ in range(1, 11):
+#     if _ == 5:
+#         break
+#     print(_)
+
+'''continue'''
+# for _ in range(1, 11):
+#     if _ == 5:
+#         continue
+#     print(_)
+
+'''pass'''
+# for _ in range(1, 11):
+#     if _ == 5:
+#         pass
+#     print(_)
+
+'''while'''
+# i = 1
+# while i < 11:
+#     print(i)
+#     i += 1
