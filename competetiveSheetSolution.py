0efe3da4134fc93e6bf3d26dcb7e04bfa9fe0ec7
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-12-13 08:47:07 +0530

    PythonProblemSheet 2 question

---
+++ competetiveSheetSolution.py
@@ -0,0 +1,33 @@
+'''
+1. Sum of Digits: Write a function that takes an integer as input and returns the sum of its digits.
+
+'''
+
+
+# def sum(a, b):
+#     sum = a + b
+#     return sum
+# a = int(input("Enter value of A:"))
+# b = int(input("Enter value of B:"))
+
+# print(f"The sum of {a} and {b} is {sum(a,b)}")
+
+
+'''
+
+2.Factorial Calculation: Create a function to calculate the factorial of a given number.
+
+'''
+
+# approach without using recurusion
+
+
+def factorial(n):
+    fact = 1
+    for i in range(1, n):
+        fact = fact + fact * i
+    return fact
+
+
+n = int(input("Enter a number to calculate factorial:"))
+print(f"The factorial of {n} is {factorial(n)}")
